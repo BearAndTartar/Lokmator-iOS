bb96dac55d12f3efafafd77f78c45412f82da069
Ozgun Zor <ozgunemrezor@gmail.com>
2017-05-28 03:45:40 +0300

    Main screen collectionViewCell class is added.

---
+++ Lokmator/Screens/Main/Views/LokmaCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  LokmaCollectionViewCell.swift
+//  Lokmator
+//
+//  Created by Ozgun Zor on 28/05/2017.
+//  Copyright © 2017 Ozgun Zor. All rights reserved.
+//
+
+import UIKit
+
+class LokmaCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+}
